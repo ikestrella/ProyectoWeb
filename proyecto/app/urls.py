19581ff633734ccafefc73ada7ad76a508183578
ikestrella <ikestrella@utpl.edu.ec>
2025-01-11 13:34:14 -0500

    Cambios

--- proyecto/app/urls.py
+++ proyecto/app/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
-#from .views import listar_carreras
 from . import views
 
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout_u, name='logout'),
-    path('inicio/', views.presentar_inicio,name='inicio'),
+    path('', views.presentar_inicio, name='inicio'),
     path('perfil/', views.presentar_perfil, name='perfil'),
-    path('productos/', views.presentar_productos, name='mostrarProductos'),
-    path('obras/', views.presentar_obras, name='mostrarObras'),
+    path('perfil/obras/', views.presentar_obras, name='mostrarObras'),
     path('perfil/obra/agregar/', views.agregar_obra, name='agregarObra'),
-    path('perfil/producto/agregar/', views.agregar_producto, name='agregarProducto')
+    path('perfil/obra/editar/<int:obra_id>/', views.editar_obra, name='editarObra'),
+    path('perfil/obra/eliminar/', views.eliminar_obra, name='eliminarObra'),
+    path('perfil/productos/', views.presentar_productos, name='mostrarProductos'),
+    path('perfil/producto/agregar/', views.agregar_producto, name='agregarProducto'),
+    path('perfil/producto/editar/<int:producto_id>/', views.editar_producto, name='editarProducto'),
+    path('perfil/producto/eliminar/', views.eliminar_producto, name='eliminarProducto')
 ]
-
--- proyecto/app/views.py
+++ proyecto/app/views.py
@@ -1,21 +1,20 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
+from django.contrib import messages
+from django.urls import reverse
 from .models import Artista, Obra, Producto
-
 
 def logout_u(request):
     logout(request)
     return redirect('login')
-
 
 def presentar_perfil(request):
     usuario = request.session.get('usuario')
     artista = Artista.objects.get(usuario=usuario)
     obras = Obra.objects.filter(artista=artista)
     productos = Producto.objects.filter(artista=artista)    
-    return render(request, 'pages/perfil/artista.html', {'obras': obras,'productos': productos,'artista':artista})
-
+    return render(request, 'pages/perfil/artista.html', {'obras': obras, 'productos': productos, 'artista': artista})
 
 def login(request):
     if request.method == 'POST':
@@ -27,10 +26,9 @@ def login(request):
         except:
             return render(request, 'pages/login.html', {'error': 'Usuario no encontrado.'})
 
-        if artista.contrasenia == contrasenia:
-                request.session['usuario'] = artista.usuario
-
-                return redirect('perfil')
+        if contrasenia == artista.contrasenia:
+            request.session['usuario'] = artista.usuario
+            return redirect('perfil')
         else:
             return render(request, 'pages/login.html', {'error': 'Contraseña incorrecta.'})
             
@@ -91,7 +89,7 @@ def agregar_producto(request):
             producto.save()
             return redirect('perfil')
         except Exception as e:
-            print(f"Error al guardar la obra: {e}")
+            print(f"Error al guardar el producto: {e}")
             
     return render(request, 'pages/perfil/agregar-producto.html')
 
@@ -107,9 +105,79 @@ def presentar_productos(request):
 
     productos = Producto.objects.filter(artista=artista)
     context = {
-        'productos': productos
+        'productos': productos,
+        'artista': artista
     }
     return render(request, 'pages/perfil/producto.html', context)
+
+
+def editar_producto(request, producto_id):
+    usuario = request.session.get('usuario')
+    if not usuario:
+        return redirect('login')
+
+    try:
+        artista = Artista.objects.get(usuario=usuario)
+    except Artista.DoesNotExist:
+        return HttpResponse("El artista no existe.", status=404)
+
+    producto = get_object_or_404(Producto, id=producto_id, artista=artista)
+
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        descripcion = request.POST.get('descripcion')
+        precio = request.POST.get('precio')
+        imagen = request.FILES.get('imagen')
+
+        producto.nombre = nombre
+        producto.descripcion = descripcion
+        producto.precio = precio
+        
+        # Handle image upload
+        if imagen:
+            producto.imagen = imagen
+        else:
+            # If no image was uploaded, keep the existing one
+            messages.add_message(request, messages.WARNING, 'Se mantuvo la imagen actual debido a que no se seleccionó una nueva.')
+
+        try:
+            producto.save()
+            messages.add_message(request, messages.SUCCESS, 'Producto actualizado con éxito.')
+        except Exception as e:
+            print("Error saving product:", str(e))
+            messages.add_message(request, messages.ERROR, 'Hubo un error al actualizar el producto: ' + str(e))
+        
+        return redirect('mostrarProductos')
+
+    context = {
+        'producto': producto,
+        'artista': artista
+    }
+    return render(request, 'pages/perfil/producto.html', context)
+
+
+def eliminar_producto(request):
+    usuario = request.session.get('usuario')
+    if not usuario:
+        return redirect('login')
+
+    try:
+        artista = Artista.objects.get(usuario=usuario)
+    except Artista.DoesNotExist:
+        return HttpResponse("El artista no existe.", status=404)
+
+    if request.method == 'GET':
+        producto_id = request.GET.get('id')
+        if producto_id:
+            try:
+                producto = Producto.objects.get(id=producto_id, artista=artista)
+                producto.delete()
+                messages.add_message(request, messages.SUCCESS, 'Producto eliminado con éxito.')
+            except Producto.DoesNotExist:
+                messages.add_message(request, messages.ERROR, 'El producto no existe o no pertenece al artista.')
+        return redirect('mostrarProductos')
+    else:
+        return HttpResponse("Método no permitido", status=405)
 
 def presentar_obras(request):
     usuario = request.session.get('usuario')
@@ -123,6 +191,72 @@ def presentar_obras(request):
 
     obras = Obra.objects.filter(artista=artista)
     context = {
-        'obras': obras
+        'obras': obras,
+        'artista': artista
     }
     return render(request, 'pages/perfil/obra.html', context)
+
+def editar_obra(request, obra_id):
+    usuario = request.session.get('usuario')
+    if not usuario:
+        return redirect('login')
+
+    try:
+        artista = Artista.objects.get(usuario=usuario)
+    except Artista.DoesNotExist:
+        return HttpResponse("El artista no existe.", status=404)
+
+    obra = get_object_or_404(Obra, id=obra_id, artista=artista)
+
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        descripcion = request.POST.get('descripcion')
+        imagen = request.FILES.get('imagen')  # Obtener el archivo de imagen si se subió
+
+        obra.nombre = nombre
+        obra.descripcion = descripcion
+        
+        # Handle image upload
+        if imagen:
+            obra.imagen = imagen  # Asignar la nueva imagen si se proporcionó una
+        else:
+            # Si no se subió una imagen, mantener la actual
+            messages.add_message(request, messages.WARNING, 'Se mantuvo la imagen actual debido a que no se seleccionó una nueva.')
+
+        try:
+            obra.save()
+            messages.add_message(request, messages.SUCCESS, 'Obra actualizada con éxito.')
+        except Exception as e:
+            print("Error saving obra:", str(e))
+            messages.add_message(request, messages.ERROR, 'Hubo un error al actualizar la obra: ' + str(e))
+        
+        return redirect('mostrarObras')
+
+    context = {
+        'obra': obra,
+        'artista': artista
+    }
+    return render(request, 'pages/perfil/editar_obra.html', context)
+
+def eliminar_obra(request):
+    usuario = request.session.get('usuario')
+    if not usuario:
+        return redirect('login')
+
+    try:
+        artista = Artista.objects.get(usuario=usuario)
+    except Artista.DoesNotExist:
+        return HttpResponse("El artista no existe.", status=404)
+
+    if request.method == 'GET':
+        obra_id = request.GET.get('id')
+        if obra_id:
+            try:
+                obra = Obra.objects.get(id=obra_id, artista=artista)
+                obra.delete()
+                messages.add_message(request, messages.SUCCESS, 'Obra eliminada con éxito.')
+            except Obra.DoesNotExist:
+                messages.add_message(request, messages.ERROR, 'La obra no existe o no pertenece al artista.')
+        return redirect('mostrarObras')
+    else:
+        return HttpResponse("Método no permitido", status=405)
